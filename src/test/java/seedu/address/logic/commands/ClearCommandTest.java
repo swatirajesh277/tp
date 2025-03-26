cebf97ce37969c38f55c4c59c88e93c31c94990e
Jun Xu <E1122727@u.nus.edu>
2025-03-26 17:50:30 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -1,17 +1,14 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
-import javafx.application.Platform;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.ui.ClearConfirmationWindow;
 import seedu.address.ui.ClearConfirmationWindowStub;
 
 public class ClearCommandTest {
