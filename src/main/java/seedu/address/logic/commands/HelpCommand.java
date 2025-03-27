64041a38f08969543448a325ddf84ec6183802d8
weiwong834 <weiyin111604@gmail.com>
2025-03-27 12:06:45 +0800

    Add log command usage to HelpWindow

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -16,38 +16,42 @@ public class HelpCommand extends Command {
             + "Example: " + COMMAND_WORD;
 
     public static final List<String> HELP_COMMANDS = Arrays.asList(
-            "Add: Add a new student in Prof-iler.\n"
+            "add: Add a new student in Prof-iler.\n"
                     + "Usage: add NAME p/PHONE id/ID e/EMAIL pr/PROJECT [t/TAG]...\n"
                     + "Example: add John Doe p/98765432 id/A0293712D e/john@example.com pr/Orbital t/Y3 t/CS",
 
-            "Delete: Remove student(s) by index number.\n"
+            "delete: Remove student(s) by index number.\n"
                     + "Usage: delete INDEX [MORE_INDEXES] (separated by commas)\n"
                     + "Example: delete 1, 3",
 
-            "Edit: Modify student details by index.\n"
+            "edit: Modify student details by index.\n"
                     + "Usage: edit INDEX [n/NAME] [id/ID] [p/PHONE] [e/EMAIL] [pr/PROJECT] [t/TAG]...\n"
                     + "Example: edit 1 p/91234567 e/john@example.com",
 
-            "Progress: Update a student's project progress by index.\n"
+            "progress: Update a student's project progress by index.\n"
                     + "Usage: progress INDEX pb/PROGRESS\n"
                     + "Example: progress 1 pb/87",
 
-            "Find: Search students by name (case-insensitive).\n"
+            "log: Adds log for student by index.\n" +
+                    "Format: log INDEX l/LOG\n" +
+                    "Example: log 1 l/CS2103T tutor",
+
+            "find: Search students by name (case-insensitive).\n"
                     + "Usage: find KEYWORD [MORE_KEYWORDS]...\n"
                     + "Example: find John Doe",
 
-            "Filter: Display students by specific project or tag.\n"
+            "filter: Display students by specific project or tag.\n"
                     + "Usage: filter [pr/PROJECT] | [t/TAG]...\n"
                     + "Examples: filter pr/Orbital, filter t/Y3",
 
-            "Sort: Organize students by progress in ascending or descending order.\n"
+            "sort: Organize students by progress in ascending or descending order.\n"
                     + "Usage: sort asc|desc\n"
                     + "Example: sort desc",
 
-            "List: Show all students.\n"
+            "list: Show all students.\n"
                     + "Usage: list",
 
-            "Clear: Remove all students from Prof-iler.\n"
+            "clear: Remove all students from Prof-iler.\n"
                     + "Usage: clear\n"
     );
 
