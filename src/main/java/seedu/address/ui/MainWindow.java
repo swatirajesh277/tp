ca582ff34a01f681c9dc8ca105026374a1fc5479
Jun Xu <E1122727@u.nus.edu>
2025-03-31 16:09:16 +0800

    Fix issue of clearconfirmationwindow not closing when main program is closed first

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -34,6 +34,7 @@ public class MainWindow extends UiPart<Stage> {
     private PersonListPanel personListPanel;
     private ResultDisplay resultDisplay;
     private HelpWindow helpWindow;
+    private ClearConfirmationWindow clearConfirmationWindow;
 
     @FXML
     private StackPane commandBoxPlaceholder;
@@ -66,6 +67,7 @@ public class MainWindow extends UiPart<Stage> {
         setAccelerators();
 
         helpWindow = new HelpWindow();
+        clearConfirmationWindow = ClearConfirmationWindow.getInstance();
     }
 
     public Stage getPrimaryStage() {
@@ -78,6 +80,7 @@ public class MainWindow extends UiPart<Stage> {
 
     /**
      * Sets the accelerator of a MenuItem.
+     *
      * @param keyCombination the KeyCombination value of the accelerator
      */
     private void setAccelerator(MenuItem menuItem, KeyCombination keyCombination) {
@@ -161,6 +164,9 @@ public class MainWindow extends UiPart<Stage> {
         logic.setGuiSettings(guiSettings);
         helpWindow.hide();
         primaryStage.hide();
+        if (clearConfirmationWindow.isShowing()) {
+            clearConfirmationWindow.hide();
+        }
     }
 
     public PersonListPanel getPersonListPanel() {
