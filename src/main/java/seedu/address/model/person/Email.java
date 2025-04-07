3baafbf101d68ea2a78b7747f54cb1e011ba4337
weiwong834 <weiyin111604@gmail.com>
2025-04-07 15:23:02 +0800

    Update correct error messages

--- src/main/java/seedu/address/model/person/Email.java
+++ src/main/java/seedu/address/model/person/Email.java
@@ -14,13 +14,16 @@ public class Email {
             + "and adhere to the following constraints:\n"
             + "1. The local-part should only contain alphanumeric characters and these special characters, excluding "
             + "the parentheses, (" + SPECIAL_CHARACTERS + "). The local-part may not start or end with any special "
-            + "characters.\n"
+            + "characters and special characters should not be consecutive.\n"
             + "2. This is followed by a '@' and then a domain name. The domain name is made up of domain labels "
             + "separated by periods.\n"
             + "The domain name must:\n"
-            + "    - end with a domain label at least 2 characters long\n"
-            + "    - have each domain label start and end with alphanumeric characters\n"
-            + "    - have each domain label consist of alphanumeric characters, separated only by hyphens, if any.";
+            + "    - end with a domain label at least 2 characters long.\n"
+            + "    - have each domain label start and end with alphanumeric characters.\n"
+            + "    - have each domain label consist of alphanumeric characters, separated only "
+            + "by hyphens (non-consecutive), if any.\n"
+            + "    - A top-level domain (e.g. `.com`) is optional.";
+
     // alphanumeric and special characters
     private static final String ALPHANUMERIC_NO_UNDERSCORE = "[^\\W_]+"; // alphanumeric characters except underscore
     private static final String LOCAL_PART_REGEX = "^" + ALPHANUMERIC_NO_UNDERSCORE + "([" + SPECIAL_CHARACTERS + "]"
